ce362b8a7f5da7a47c626868e5f9ff978a34f7d8
Dan Andersson <760686+daon@users.noreply.github.com>
2018-10-28 22:15:48 +0100

    add solution

--- src/main/java/bowlinggame/Game.java
+++ src/main/java/bowlinggame/Game.java
@@ -5,11 +5,48 @@ package bowlinggame;
  *
  */
 public class Game {
+    private final int[] rolls = new int[21];
+    private int currentRoll = 0;
+
     public void roll(int pins) {
-        throw new UnsupportedOperationException();
+        this.rolls[this.currentRoll++] = pins;
     }
 
     public int getScore() {
-        throw new UnsupportedOperationException();
+        int score = 0;
+        int frameIndex = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (this.isStrike(frameIndex)) {
+                score += 10 + this.strikeBonus(frameIndex);
+                frameIndex++;
+            } else if (this.isSpare(frameIndex)) {
+                score += 10 + this.spareBonus(frameIndex);
+                frameIndex += 2;
+            } else {
+                score += this.sumOfPinsInFrame(frameIndex);
+                frameIndex += 2;
+            }
+        }
+        return score;
+    }
+
+    private int sumOfPinsInFrame(int frameIndex) {
+        return this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+    }
+
+    private int strikeBonus(int frameIndex) {
+        return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+    }
+
+    private int spareBonus(int frameIndex) {
+        return this.rolls[frameIndex + 2];
+    }
+
+    private boolean isStrike(int frameIndex) {
+        return this.rolls[frameIndex] == 10;
+    }
+
+    private boolean isSpare(int frameIndex) {
+        return this.rolls[frameIndex] + this.rolls[frameIndex + 1] == 10;
     }
 }
--- src/test/java/bowlinggame/GameTest.java
+++ src/test/java/bowlinggame/GameTest.java
@@ -16,10 +16,56 @@ public class GameTest {
 
     @Test
     public void allRollsInTheGutter() {
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
+        manyRolls(20, 0);
 
         assertEquals(game.getScore(), 0);
+    }
+
+    @Test
+    public void allRollsHitOnePin() {
+        manyRolls(20, 1);
+
+        assertEquals(game.getScore(), 20);
+    }
+
+    @Test
+    public void rollsOneSpare() {
+        rollSpare();
+        game.roll(3);
+        manyRolls(17, 0);
+
+        assertEquals(game.getScore(), 16);
+    }
+
+    @Test
+    public void rollsOneStrike() {
+        rollStrike();
+        game.roll(4);
+        game.roll(3);
+        manyRolls(16, 0);
+
+        assertEquals(game.getScore(), 24);
+    }
+
+    @Test
+    public void rollsPerfectGame() {
+        manyRolls(20, 10);
+
+        assertEquals(game.getScore(), 300);
+    }
+
+    private void manyRolls(int rolls, int pins) {
+        for (int i = 0; i < rolls; i++) {
+            game.roll(pins);
+        }
+    }
+
+    private void rollSpare() {
+        game.roll(5);
+        game.roll(5);
+    }
+
+    private void rollStrike() {
+        game.roll(10);
     }
 }
